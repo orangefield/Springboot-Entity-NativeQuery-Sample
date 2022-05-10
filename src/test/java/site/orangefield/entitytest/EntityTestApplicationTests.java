16e35dda00ac282a12ab7c8e9a433bc45e99d07a
field <jsw_777@naver.com>
2022-05-10 15:29:44 +0900

    Initial Project

---
+++ src/test/java/site/orangefield/entitytest/EntityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.orangefield.entitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
